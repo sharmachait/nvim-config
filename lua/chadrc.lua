9873e785c0a8dde8204596f5b46db87236d53700
Chaitanya Sharma <chait8126@gmail.com>
2025-12-27 10:18:25 +0000

    rust

---
+++ lua/chadrc.lua
@@ -0,0 +1,32 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "oceanic-next",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+-- M.nvdash = { load_on_startup = true }
+-- M.ui = {
+--       tabufline = {
+--          lazyload = false
+--      }
+-- }
+--M.ui = {
+--  statusline = {
+--    enabled = false,  -- Disable bottom statusline
+--  },
+  --tabufline = {
+    --enabled = true,
+--    lazyload = false,
+  --},
+--}
+return M
---
+++ lua/mappings.lua
@@ -0,0 +1,43 @@
+require "nvchad.mappings"
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", {desc="Toggle nvim-tree"})
+
+-- Rust debugging mappings - using <leader>b prefix
+map("n", "<leader>bb", function() require('dap').toggle_breakpoint() end, { desc = "Toggle breakpoint" })
+map("n", "<leader>br", "<cmd>RustLsp debuggables<cr>", { desc = "Rust debuggables" })
+map("n", "<leader>bc", function() require('dap').continue() end, { desc = "Continue" })
+map("n", "<leader>bs", function() require('dap').step_over() end, { desc = "Step over" })
+map("n", "<leader>bi", function() require('dap').step_into() end, { desc = "Step into" })
+map("n", "<leader>bo", function() require('dap').step_out() end, { desc = "Step out" })
+map("n", "<leader>bt", function() require('dap').terminate() end, { desc = "Terminate" })
+map("n", "<leader>bu", function() require('dapui').toggle() end, { desc = "Toggle DAP UI" })
+
+map("n", "<leader>bd", function()
+  -- Build first
+  vim.fn.system('cargo build')
+  
+  -- Get the binary name from Cargo.toml or use default
+  local binary_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')  -- Gets folder name
+  local program = vim.fn.getcwd() .. '/target/debug/' .. binary_name
+  
+  -- Ask for arguments
+  local args_string = vim.fn.input('Program arguments: ')
+  local args = {}
+  if args_string ~= "" then
+    args = vim.split(args_string, " ")
+  end
+  
+  -- Start debugging
+  require('dap').run({
+    type = 'codelldb',
+    request = 'launch',
+    name = 'Debug',
+    program = program,
+    args = args,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  })
+end, { desc = "Build and debug with args" })
